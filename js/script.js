44b47f61da498e9b365624eb5ee1d4e7e8ecf4c3
Ollie Grove <ollie_grove@protonmail.com>
2022-07-18 12:45:00 -0700

    Fetched user API data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 //Variable to select "overview" class div, where profile information will appear
+
 const overview = document.querySelector(".overview"); 
 
 const username = "ollie-grove"; 
@@ -6,28 +7,31 @@ const username = "ollie-grove";
 //Async function to fetch information from GitHub profile. Target the "users" endpoint and use temperate literal to add the global username variable to the endpoint. 
 
 const gitUserInfo = async function () {
-    const userInfo = await fetch(`https://api.github.com/users/${ollie-grove}`); 
+    const userInfo = await fetch(`https://api.github.com/users/${username}`); 
 
     const data = await userInfo.json();
-    
+    //console.log(data); 
+    displayInfo(data); 
 };
 
-console.log(gitUserInfo); 
-
+gitUserInfo();
 
 //Function to display the fetched user information on the page. Should accept the json data as a parameter.
 
-const displayUserInfo = function (data) {
-    const div = document.createElement(".user-info"); 
-    div.innerHTML = `
-    <figure>
-    <img alt="user avatar" src=${} />
-  </figure>
-  <div>
-    <p><strong>Name:</strong> ${}</p>
-    <p><strong>Bio:</strong> ${}</p>
-    <p><strong>Location:</strong> ${}</p>
-    <p><strong>Number of public repos:</strong> ${}</p>
-  </div> `
+const displayInfo = function (data) {
+    const div = document.createElement("div"); 
+    div.classList.add("user-info"); 
 
-}
+    div.innerHTML = `
+        <figure>
+            <img alt = "user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong>${data.name}</p>
+            <p><strong>Bio:</strong>${data.bio}</p>
+            <p><strong>Location:</strong>${data.location}</p>
+            <p><strong>Number of public repos:</strong>${data.public_repos}</p>
+        </div>
+    `;
+    overview.append(div);
+};
