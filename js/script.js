4549767e837fe26f230817a8674d3dec3535602e
Ollie Grove <ollie_grove@protonmail.com>
2022-07-21 13:54:28 -0600

    Added parameters to the API call and created async function to display repos

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const overview = document.querySelector(".overview");
 
 const username = "ollie-grove"; 
 
+const repoList = document.querySelector(".repo-list"); 
+
 //Async function to fetch information from GitHub profile. Target the "users" endpoint and use temperate literal to add the global username variable to the endpoint. 
 
 const gitUserInfo = async function () {
@@ -12,6 +14,7 @@ const gitUserInfo = async function () {
     const data = await userInfo.json();
     //console.log(data); 
     displayInfo(data); 
+    
 };
 
 gitUserInfo();
@@ -34,4 +37,25 @@ const displayInfo = function (data) {
         </div>
     `;
     overview.append(div);
+    gitRepos(); 
+     
 };
+
+//Function to fetch repos. Find the parameters to sort repos by most recently updated to least updated (sort=updated), show up to 100 repos per page at a time(&per_page=100)
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json(); 
+    //console.log(repoData); 
+    displayRepos(repoData);
+}; 
+
+const displayRepos = function(repos) {
+    for (repo of repos) {
+        const repoItem = document.createElement("li"); 
+        repoItem.classList.add("repo"); 
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`; 
+        repoList.append(repoItem); 
+    }
+};
+
