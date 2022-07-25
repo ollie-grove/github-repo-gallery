673cf94f65d2ec59d21bc118f92f3077e45cf803
Ollie Grove <ollie_grove@protonmail.com>
2022-07-25 17:27:29 -0600

    Added click event, created function to get specific repo info

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ const username = "ollie-grove";
 
 const repoList = document.querySelector(".repo-list"); 
 
+const allRepos = document.querySelector(".repos"); 
+
+const repoData = document.querySelector(".repo-data"); 
+
 //Async function to fetch information from GitHub profile. Target the "users" endpoint and use temperate literal to add the global username variable to the endpoint. 
 
 const gitUserInfo = async function () {
@@ -50,6 +54,8 @@ const gitRepos = async function () {
     displayRepos(repoData);
 }; 
 
+
+//Display info about each repo. Use repos as a parameter so the function accepts the data returned from the last API call. Inside the function, loop and create a list item for each repo and give each item a class of repo and an h3 element. 
 const displayRepos = function(repos) {
     for (repo of repos) {
         const repoItem = document.createElement("li"); 
@@ -57,5 +63,54 @@ const displayRepos = function(repos) {
         repoItem.innerHTML = `<h3>${repo.name}</h3>`; 
         repoList.append(repoItem); 
     }
+};
+
+//Add an event listener called repoList for a click event on the unordered list with a class of "repo-list." Add a conditional statement to check if the event target matches the h3 element (name of the repo). In the conditional statement, create variable called repoName to target the innerText where the event happns. Log out the variable to the console. 
+
+
+repoList.addEventListener("click", function (e) {
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText; 
+        getRepoInfo(repoName); 
+        }
+    }); 
+
+const getRepoInfo = async function(repoName) {
+    const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`); 
+    const repoInfo = await fetchInfo.json(); 
+    //console.log(repoInfo); 
+
+    //grab languages
+
+    const fetchLanguages = await fetch(repoInfo.languages_url); 
+    const languageData = await fetchLanguages.json(); 
+    
+    //list of language - Add each language to an empty array called "languages", the languageData is an oject, you'll want to add the languages to the end of the array.
+
+    const languages = []; 
+    for (const language in languageData) {
+        languages.push(language); 
+    }
+
+    displayRepoInformation(repoInfo, languages); 
+};
+
+//Function to display the specific repo information, should accept both parameters "repoInfo" and "languages". Inside the function, empty the html of the section class "repo-data" where the individual repo data will appear. Create a new div element and add the selected repository's name, description, default branch, and link to its code on GitHub. 
+
+const displayRepoInformation = function (repoInfo, languages) {
+    repoData.innerHTML = ""; 
+    repoData.classList.remove("hide"); 
+    allRepos.classList.add("hide"); 
+
+    const div = document.createElement("div"); 
+    div.innerHTML = `
+        <h3>Name: ${repoInfo.name}</h3>
+        <p>Description: ${repoInfo.description}</p>
+        <p>Default Branch: ${repoInfo.default_branch}</p>
+        <p>Languages: ${languages.join(", ")}</p>
+        <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on Github!</a>
+    `;
+    repoData.append(div); 
+
 };
 
